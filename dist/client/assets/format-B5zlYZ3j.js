752eb11609db402b3f93a46f4333106c08cf4046
macauleychukwuyenum-spec <macauleychukwuyenum@gmail.com>
2026-06-12 23:50:18 +0100

    Delete dist directory

--- dist/client/assets/format-B5zlYZ3j.js
+++
@@ -1,1 +0,0 @@
-function t(r){const e=typeof r=="string"?Number(r):r??0;return new Intl.NumberFormat("en-NG",{style:"currency",currency:"NGN",maximumFractionDigits:0}).format(Number.isFinite(e)?e:0)}export{t as f};
--- dist/client/assets/route-CQDWFccW.js
+++
@@ -1,1 +0,0 @@
-import{j as t,O as o}from"./index-XCaju1tX.js";const n=()=>t.jsx(o,{});export{n as component};
